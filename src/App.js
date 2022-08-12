e647fa285dc9103f9e0ed1906a09269e7ee6473d
ryojif2 <ryojifuj2@gmail.com>
2022-08-12 14:10:02 +0800

    First commit, with firebase & react npms installed and configured

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import {
+  onChildAdded,
+  push,
+  ref as dbRef,
+  set,
+  update,
+  onChildChanged,
+} from "firebase/database";
+import { database, storage } from "./DB/firebase";
+import {
+  // getStorage,
+  getDownloadURL,
+  ref as sRef,
+  uploadBytes,
+} from "firebase/storage";
+import { auth } from "./DB/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
