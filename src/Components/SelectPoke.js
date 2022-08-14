8589cf2fb8dbb09a976066b6938a990c7f624b2a
ryojif2 <ryojifuj2@gmail.com>
2022-08-14 21:56:18 +0800

    First working version of pokedex, selectPoke and MainPage

---
+++ src/Components/SelectPoke.js
@@ -0,0 +1,50 @@
+import React from "react";
+import axios from "axios";
+
+const SelectPoke = (props) => {
+  const chosenPokemon = props.selectedPokemon;
+  const {
+    pokemonHP,
+    pokemonImageFront,
+    pokemonMoves,
+    pokemonMovesURL,
+    pokemonName,
+    pokemonType,
+  } = chosenPokemon;
+  const PokeName = pokemonName.toUpperCase();
+  const PokeType = pokemonType.map((type) => <h5>{type}</h5>);
+  const PokeMoves = pokemonMoves.map((move) => <h5>{move}</h5>);
+  console.log(pokemonMovesURL);
+
+  const playerAttackArray = [];
+  pokemonMovesURL.map((url) => {
+    axios.get(url).then((response) => {
+      const { name, power } = response.data;
+
+      console.log(name);
+      console.log(power);
+      playerAttackArray.push(power);
+      return playerAttackArray;
+    });
+  });
+
+  console.log(playerAttackArray);
+
+  return (
+    <div key={chosenPokemon} name={pokemonName}>
+      <img
+        style={{ height: "30vh" }}
+        src={pokemonImageFront}
+        alt={pokemonImageFront}
+        name={pokemonName}
+      />
+      <h4>{PokeName}</h4>
+      <h4>{PokeType}</h4>
+      <h4>HP: {pokemonHP}</h4>
+      <h4>Moves: {PokeMoves}</h4>
+      <button>Confirm</button>
+      <button>Back to Main Pokedex</button>
+    </div>
+  );
+};
+export default SelectPoke;
