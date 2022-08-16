eb79388aa0499150ed00630366b4198be52c7836
root <root@LAPTOP-C6553LAC.localdomain>
2022-08-16 17:02:13 +0800

    changes

--- src/DB/firebase.js
+++ src/DB/firebase.js
@@ -8,7 +8,6 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  // The value of `databaseURL` depends on the location of the database
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
