7acfcae9002c818f97e1000f89c2f1f2bdd6985a
root <root@LAPTOP-C6553LAC.localdomain>
2022-08-28 10:32:21 +0800

    saturday

--- src/DB/firebase.js
+++ src/DB/firebase.js
@@ -8,14 +8,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDqWw0yjC9gk6C2SC_HZQffUZOXQydgC0I",
-  authDomain: "project2-pokem-bootcamp.firebaseapp.com",
-  databaseURL:
-    "https://project2-pokem-bootcamp-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "project2-pokem-bootcamp",
-  storageBucket: "project2-pokem-bootcamp.appspot.com",
-  messagingSenderId: "1036526742191",
-  appId: "1:1036526742191:web:883ea8e38296fa10f2425a",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // TODO: Add SDKs for Firebase products that you want to use
